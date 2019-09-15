c7e5c212e3583db0f7864cb0e0c39a6ae5711ba5
BART! <bart@bartweb.cz>
2019-09-15 16:55:50 +0200

    feat(scrolltop): init scrolltop functions

--- projects/ngx-scrolltop/src/lib/ngx-scrolltop.component.ts
+++ projects/ngx-scrolltop/src/lib/ngx-scrolltop.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  HostListener,
+  Inject,
+  ElementRef,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'scrolltop-ngx-scrolltop',
-  template: `
-    <p>
-      ngx-scrolltop works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './ngx-scrolltop.component.html',
+  styleUrls: ['./ngx-scrolltop.component.scss'],
 })
 export class NgxScrolltopComponent implements OnInit {
+  public show = false;
+  private scrolledFromTop = false;
+  private scrollOffset: number;
 
-  constructor() { }
+  @ViewChild('scrollTopButton', { static: false }) scrollTopButton: ElementRef;
 
-  ngOnInit() {
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    if (this.document.documentElement.scrollTop === 0) {
+      this.show = false;
+      this.scrolledFromTop = false;
+    }
+
+    if (
+      this.scrolledFromTop &&
+      this.scrollOffset > this.document.documentElement.scrollTop
+    ) {
+      this.show = true;
+    }
+
+    if (
+      typeof window !== 'undefined' &&
+      this.document.documentElement.scrollTop > window.innerHeight * 2
+    ) {
+      this.scrolledFromTop = true;
+      this.scrollOffset = this.document.documentElement.scrollTop;
+    }
   }
 
+  constructor(@Inject(DOCUMENT) private document: any) {}
+
+  ngOnInit() {}
+
+  scrollToTop() {
+    if (typeof window !== 'undefined') {
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    }
+  }
 }
