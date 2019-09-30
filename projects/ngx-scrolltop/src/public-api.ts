4fd1273c22b936e91e964594a3996ec4651a1e47
BART! <bart@bartweb.cz>
2019-09-30 07:45:16 +0200

    Auto-generated commit

--- projects/ngx-scrolltop/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-scrolltop
- */
-
-export * from './lib/ngx-scrolltop.component';
-export * from './lib/ngx-scrolltop.directive';
-export * from './lib/ngx-scrolltop.module';
