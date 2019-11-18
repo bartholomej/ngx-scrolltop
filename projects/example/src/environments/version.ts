6270a43e8c6c26dd9586794d261b46c817931149
BART! <bart@bartweb.cz>
2019-11-18 17:26:32 +0100

    chore(deps): ng update + other deps

--- projects/example/src/environments/version.ts
+++ projects/example/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "TAG": "0.5.7",
+    "TAG": "0.5.8",
     "COMMIT_HASH": ""
 };
 /* tslint:enable */
