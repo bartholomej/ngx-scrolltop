ab65fb6961655d3f04090270d99e9c0a392b038e
BART! <bart@bartweb.cz>
2020-02-14 17:58:02 +0100

    chore(deps): ng9 update
    chore(deps): ng9 update

--- projects/example/src/environments/version.ts
+++ projects/example/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "TAG": "0.6.0",
+    "TAG": "0.7.1",
     "COMMIT_HASH": ""
 };
 /* tslint:enable */
