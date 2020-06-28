bec72b6c51a0fb287231d37b14161a8894751cf6
BART! <bart@bartweb.cz>
2020-06-28 20:50:07 +0200

    chore(deps): ng update 10.0.1

--- projects/example/src/environments/version.ts
+++ projects/example/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '0.8.1',
-  COMMIT_HASH: ''
+  TAG: '0.8.4',
+  COMMIT_HASH: '',
 };
 /* tslint:enable */
