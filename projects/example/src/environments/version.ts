38d07d82c3c4f1b6881fb2ed88c299a14220664e
BART! <bart@bartweb.cz>
2020-06-29 14:26:52 +0200

    chore(update): beta release 1.0.0-rc.1 β

--- projects/example/src/environments/version.ts
+++ projects/example/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '0.8.4',
+  TAG: '1.0.0-rc.1',
   COMMIT_HASH: '',
 };
 /* tslint:enable */
