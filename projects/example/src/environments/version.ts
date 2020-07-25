272d74dc14fb6e0f0ded8c44d91432f01e04d817
BART! <bart@bartweb.cz>
2020-07-25 10:03:57 +0200

    chore(version): ignore version file

--- projects/example/src/environments/version.ts
+++
@@ -1,7 +0,0 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = {
-  TAG: '1.0.0-rc.1',
-  COMMIT_HASH: '',
-};
-/* tslint:enable */
