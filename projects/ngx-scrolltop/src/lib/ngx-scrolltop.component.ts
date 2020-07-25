021d0331282669547cfef1975d39c61c8c5dbaf9
BART! <bart@bartweb.cz>
2020-07-25 10:05:42 +0200

    feat(component): removed symbol + added content projection (fix #1)

--- projects/ngx-scrolltop/src/lib/ngx-scrolltop.component.ts
+++ projects/ngx-scrolltop/src/lib/ngx-scrolltop.component.ts
@@ -3,19 +3,23 @@ import { NgxScrollTopCoreService } from './ngx-scrolltop.core.service';
 import {
   NgxScrollTopMode,
   NgxScrollTopPosition,
-  NgxScrollTopTheme
+  NgxScrollTopTheme,
 } from './ngx-scrolltop.interface';
 
 @Component({
   selector: 'ngx-scrolltop',
   templateUrl: './ngx-scrolltop.component.html',
-  styleUrls: ['./ngx-scrolltop.component.scss']
+  styleUrls: ['./ngx-scrolltop.component.scss'],
 })
 export class NgxScrollTopComponent {
   @Input() backgroundColor: string;
   @Input() symbolColor: string;
   @Input() size: number;
-  @Input() symbol: string;
+  @Input() public set symbol(value: string) {
+    console.error(
+      'NgxScrollTop: Symbol input `[symbol]="\'↑\'"` is deprecated.\n\r Use `Content projection` instead. E.g. <ngx-scrolltop>↑</ngx-scrolltop>.\n\r More info: https://github.com/bartholomej/ngx-scrolltop#options'
+    );
+  }
   @Input() position: NgxScrollTopPosition = 'right';
   @Input() theme: NgxScrollTopTheme = 'gray';
   @Input() mode: NgxScrollTopMode = 'classic';
