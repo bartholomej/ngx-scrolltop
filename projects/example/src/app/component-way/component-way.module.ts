afd7cd77bfbd53a072d6eb0105a87c4208976f9f
BART! <bart@bartweb.cz>
2021-01-08 19:55:12 +0100

    fix: relative imports

--- projects/example/src/app/component-way/component-way.module.ts
+++ projects/example/src/app/component-way/component-way.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgxScrollTopModule } from 'ngx-scrolltop';
+import { NgxScrollTopModule } from '../../../../ngx-scrolltop/src/lib/ngx-scrolltop.module';
 import { ComponentWayRoutingModule } from './component-way-routing.module';
 import { ComponentWayComponent } from './component-way.component';
 
 @NgModule({
   declarations: [ComponentWayComponent],
-  imports: [NgxScrollTopModule, CommonModule, ComponentWayRoutingModule]
+  imports: [NgxScrollTopModule, CommonModule, ComponentWayRoutingModule],
 })
 export class ComponentWayModule {}
--- projects/example/src/app/directive-way/directive-way.module.ts
+++ projects/example/src/app/directive-way/directive-way.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NgxScrollTopModule } from 'ngx-scrolltop';
+import { NgxScrollTopModule } from '../../../../ngx-scrolltop/src/lib/ngx-scrolltop.module';
 import { DirectiveWayRoutingModule } from './directive-way-routing.module';
 import { DirectiveWayComponent } from './directive-way.component';
 
 @NgModule({
   declarations: [DirectiveWayComponent],
-  imports: [CommonModule, DirectiveWayRoutingModule, NgxScrollTopModule]
+  imports: [CommonModule, DirectiveWayRoutingModule, NgxScrollTopModule],
 })
 export class DirectiveWayModule {}
