5f2c11e13b9b6a508ef555cdbb6d27821456f65f
Erick JA <erickjx@yahoo.com.mx>
2024-12-30 14:07:22 -0600

    Fix demo, break in Angular 19 upgrade

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { VERSION } from '../environments/version';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent {
   public title = 'NgxScrollTop demo | Angular go to top button';
