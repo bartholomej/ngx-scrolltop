7b529184e0d379c609b9b6995caafe6bba3ed4c2
BART! <bart@bartweb.cz>
2025-10-25 15:40:50 +0200

    fix(demo): zoneless app

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [provideZonelessChangeDetection()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
