202ab9e09c4cb5b2dfb45fbb1bb3bd65a1ea6dc7
BART! <bart@bartweb.cz>
2026-06-26 07:58:14 +0200

    feat(demo): target in new section

--- projects/demo/src/app/target-way/target-way.component.ts
+++ projects/demo/src/app/target-way/target-way.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgxScrollTopComponent } from './../../../../../projects/ngx-scrolltop/src/public-api';
 
 @Component({
-  selector: 'app-component-way',
-  templateUrl: './component-way.component.html',
-  styleUrls: ['./component-way.component.scss'],
+  selector: 'app-target-way',
+  templateUrl: './target-way.component.html',
+  styleUrls: ['./target-way.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgxScrollTopComponent],
 })
-export default class ComponentWayComponent {}
+export default class TargetWayComponent {
+  protected readonly messages = Array.from({ length: 50 }, (_, i) => i + 1);
+}
